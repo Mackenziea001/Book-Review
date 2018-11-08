bd403fa7de0860d30169d792afe69980ce895c08
Mackenzie E. Albitz <hqsv@iup.edu>
2018-11-08 11:30:06 -0500

    Update user.php
    changes made to send registration email

--- user.php
+++ user.php
@@ -73,22 +73,24 @@ function emailExists(){
         }  
     
     // create new user record
+
+    // create new user record
 function create(){
  
     // to get time stamp for 'created' field
     $this->created=date('Y-m-d H:i:s');
  
     // insert query
-    $query = "INSERT INTO
-                " . $this->table_name . "
+    $query = "INSERT INTO " . $this->table_name . "
             SET
-                firstname = :firstname,
-                lastname = :lastname,
-                email = :email,
-                password = :password,
-                access_level = :access_level,
-                status = :status,
-                created = :created";
+        firstname = :firstname,
+        lastname = :lastname,
+        email = :email,
+        password = :password,
+        access_level = :access_level,
+                access_code = :access_code,
+        status = :status,
+        created = :created";
  
     // prepare the query
     $stmt = $this->conn->prepare($query);
@@ -99,19 +101,22 @@ function create(){
     $this->email=htmlspecialchars(strip_tags($this->email));
     $this->password=htmlspecialchars(strip_tags($this->password));
     $this->access_level=htmlspecialchars(strip_tags($this->access_level));
+    $this->access_code=htmlspecialchars(strip_tags($this->access_code));
     $this->status=htmlspecialchars(strip_tags($this->status));
  
     // bind the values
     $stmt->bindParam(':firstname', $this->firstname);
     $stmt->bindParam(':lastname', $this->lastname);
     $stmt->bindParam(':email', $this->email);
-    
+    $stmt->bindParam(':contact_number', $this->contact_number);
+    $stmt->bindParam(':address', $this->address);
  
     // hash the password before saving to database
     $password_hash = password_hash($this->password, PASSWORD_BCRYPT);
     $stmt->bindParam(':password', $password_hash);
  
     $stmt->bindParam(':access_level', $this->access_level);
+    $stmt->bindParam(':access_code', $this->access_code);
     $stmt->bindParam(':status', $this->status);
     $stmt->bindParam(':created', $this->created);
  
@@ -122,8 +127,7 @@ function create(){
         $this->showError($stmt);
         return false;
     }
-    
-    
+ 
 }
     // read all user records
 function readAll($from_record_num, $records_per_page){
